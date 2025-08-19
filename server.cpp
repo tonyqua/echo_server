8bcbb4b70a75797cf33aaaca816f8aeb42a01c5b
Anton <dubrovinanton2216@gmail.com>
2025-08-19 13:01:10 +0300

    echo_server демка. осталось доделать recv и send

--- server.cpp
+++ server.cpp
@@ -32,9 +32,49 @@ int main (int argc, char **argv) {
     address.sin_addr.s_addr = INADDR_LOOP; // Делаем так чтобы адресс работал на всех IPv4 интерфейсах
     address.sin_port = htons(port); // Присваиваем порт к нашему адрессу
 
+    // Связываем наш сокет с адрессом и портом.
     int bind_value = bind(sock, (struct sockaddr*)&address, sizeof(address));
 
+    if (bind_value < 0) {
+        perror("bind failed. Error!");
+        return -1;
+    }
 
+    // 3 listen
+    // Ставим сокет в режим прослушивания входящих подключений
+    // listen(sock, 1) - мы слушаем данный сокет,
+    // 1 - значит, что мы слушаем сокет в одной очереди
+    int listen_value = listen(sock, 1);
+    if (listen_value < 0) {
+        perror("listen failed.");
+    }
 
+    // 4 accept
+    // Принимаем новое соединение
+    struct sockaddr_in remote_address;
+    // Беззнаковое целое число, которое сохраняет в себя длину нашего адреса
+    socklen_t remote_addrlen = sizeof(address);
+    // Функция accept() ждёт пока появится что-то для принятия в client_socket
+    int client_socket = accept(sock, (struct sockaddr*)&remote_address, &remote_addrlen);
+    // Сохраняем IP клиента
+    std::string client_ip = inet_ntoa(address.sin_addr);
+    // Сохраняем port клиента
+    int remote_port = ntohs(address.sin_port);
+
+    std::cout << "New client! @ " << client_ip << ":" << remote_port << std::endl;
+
+    int BUFFLEN = 1024;
+    char buffer[BUFFLEN];
+
+    while (true) {
+        // Очистка буффера от возможного мусора для принятия данных для клиента
+        memset(buffer, 0, BUFFLEN);
+        // 5 recv
+        // recv() функция, которая получает данные из сокета
+        // sock - место откуда мы читаем сокет. buffer - место в памяти, куда будут записаны данные,
+        // BUFFLEN - длина буффера, максимальное количство байт которое мы можем сохранить
+        int bytes_received = recv(client_socket, buffer, BUFFLEN-1, 0);
+
+    }
 
 }
