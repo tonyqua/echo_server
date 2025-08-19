d5fe01f8a4ee28aac52ba9e3afc71da7b973792a
Anton <dubrovinanton2216@gmail.com>
2025-08-19 15:05:25 +0300

    Фулл версия демки простого echo-server, который отправляет клиенту сообщение, которое он отправил серверу.

--- server.cpp
+++ server.cpp
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstring>
+
 
 int main (int argc, char **argv) {
 
@@ -28,8 +30,9 @@ int main (int argc, char **argv) {
     // 2 bind
     // Привязываем наш сокет к определённому адресу и порту на локальной машине
     struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET; // Присваиваем IPv4 нашему адресу
-    address.sin_addr.s_addr = INADDR_LOOP; // Делаем так чтобы адресс работал на всех IPv4 интерфейсах
+    address.sin_addr.s_addr = INADDR_ANY; // Делаем так чтобы адресс работал на всех IPv4 интерфейсах
     address.sin_port = htons(port); // Присваиваем порт к нашему адрессу
 
     // Связываем наш сокет с адрессом и портом.
@@ -52,9 +55,11 @@ int main (int argc, char **argv) {
     // 4 accept
     // Принимаем новое соединение
     struct sockaddr_in remote_address;
+    memset(&address, 0, sizeof(address));
     // Беззнаковое целое число, которое сохраняет в себя длину нашего адреса
     socklen_t remote_addrlen = sizeof(address);
     // Функция accept() ждёт пока появится что-то для принятия в client_socket
+    std::cout << "Waiting for new connection" << std::endl;
     int client_socket = accept(sock, (struct sockaddr*)&remote_address, &remote_addrlen);
     // Сохраняем IP клиента
     std::string client_ip = inet_ntoa(address.sin_addr);
@@ -66,6 +71,7 @@ int main (int argc, char **argv) {
     int BUFFLEN = 1024;
     char buffer[BUFFLEN];
 
+    //
     while (true) {
         // Очистка буффера от возможного мусора для принятия данных для клиента
         memset(buffer, 0, BUFFLEN);
@@ -75,6 +81,27 @@ int main (int argc, char **argv) {
         // BUFFLEN - длина буффера, максимальное количство байт которое мы можем сохранить
         int bytes_received = recv(client_socket, buffer, BUFFLEN-1, 0);
 
-    }
+        if (bytes_received < 0) {
+            perror("recv failed.");
+            return -1;
+        }
+        if (bytes_received == 0) {
+            std::cout << "Connection with " << client_ip << ":" << remote_port << " closed" << std::endl;
+            break;
+        }
+        if (buffer[bytes_received-1] == '\n') {
+            buffer[bytes_received-1] = 0;
+        }
+        std::cout << "Client message: \n" << buffer << std::endl;
+        // 6 send
+        std::string response = "Hello client at " + client_ip + ":" + std::to_string(remote_port) +
+            ". Your message was \n\"" + std::string(buffer) + "\"\n";
 
+        int bytes_sent = send(client_socket, response.c_str(), response.length(), 0);
+        if (bytes_sent < 0) {
+            perror("send failed.");
+        }
+    }
+    std::cout << "Connection closed." << std::endl;
+    shutdown(client_socket, SHUT_WR);
 }
